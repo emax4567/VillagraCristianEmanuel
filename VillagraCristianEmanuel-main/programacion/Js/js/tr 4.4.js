60232c87bf2bb173522fd27e9aa398e6139a7a37
emax4567 <164574523+emax4567@users.noreply.github.com>
2024-04-05 20:51:53 -0300

    Add files via upload

---
+++ VillagraCristianEmanuel-main/programacion/Js/js/tr 4.4.js
@@ -0,0 +1,5 @@
+alert ("ses");
+var aux = "palabra";
+var numero = 42;
+var resul = aux + numero;
+function nombres (x, aux) {} //
---
+++ VillagraCristianEmanuel-main/tp1/fn.js
@@ -0,0 +1,14 @@
+let frutas = ['pera', 'manzana','frutilla'];
+
+
+
+
+
+
+
+//for (let frutas = manzana; index < array.length; index++) {
+//    const element = frutas[index]; (incompleto)
+//frutas.forEach(fruta => {
+ //   console.log (fruta)
+//});
+//console.log (frutas[1]);
