48c4cb9f4a9076cad634a64b4e5a00d84629462e
emax4567 <164574523+emax4567@users.noreply.github.com>
2024-04-19 15:32:35 -0300

    Update json.js

--- tp1/json.js
+++ tp1/json.js
@@ -1,4 +1,3 @@
-
 const personaje={
     "id": 140,
     "name": "Genital Washer",
@@ -21,13 +20,13 @@ const personaje={
     "url": "https://rickandmortyapi.com/api/character/140",
     "created": "2017-12-27T18:47:44.566Z"
 }
-console.log(personaje);
-function buscar(){
-var n=getElementById("numero").value;
-if (personaje.id==n) {
-    document.getElementById("result"). innerHTML="personaje " personaje.name " esta "personaje.status
-} else {
-    document.getElementById("result"). innerHTML= "personaje no resgistrado"
-}
+console.log(personaje) 
+    function buscar() {
+    var n=document.getElementById("numero").value;
+    if (personaje.id==n) {
+        document.getElementById("result"). innerHTML="el personaje "+personaje.name+" esta "+personaje.status
+    } else {
+        document.getElementById("result"). innerHTML="el personaje no esta registrado"
+    }
 }
 //fetch ('uhttps://rickandmortyapi.com/api/character/2')
